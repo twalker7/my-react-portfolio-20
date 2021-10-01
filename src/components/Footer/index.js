335f367641c9ac0402dad038e9ab33be109da2b0
Thomas Walker <thomasnwalker@yahoo.com>
2021-09-30 18:03:50 -0700

    added linkedin

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     return(
         <h2> 
-            <a target="_blank" href="https://github.com/twalker7">Github </a> | <a href="">LinkedIn</a> 
+            <a target="_blank" href="https://github.com/twalker7">Github </a> | <a href="https://www.linkedin.com/in/thomas-walker-7360ba8a/">LinkedIn</a> 
         </h2>
     );
 }
