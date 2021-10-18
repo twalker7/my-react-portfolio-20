7a60bfe3a70ae3c138e0842f789e432f20c95fc5
Thomas Walker <thomasnwalker@yahoo.com>
2021-10-18 02:32:01 -0700

    profile photograph changed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 function About(){
     return(
         <div>
-            <img className="profile-image" src ={require(`../../assets/profile-photo.jpeg`).default}/>
-
-            <h3> 
+            <img className="profile-image" src ={require(`../../assets/professional-thumbnail-potential.jpg`).default}/>
+           
+            <h4> 
             
-            Full Stack Web Developer with experience in multiple programming languages and a passion for problem-solving. A chess teacher with strong communication skills and a collaborative spirit, experienced in handling and conveying complex technical concepts. Polished analytical skills for effective debugging and algorithm design.             </h3>
+            Full Stack Web Developer with experience in multiple programming languages and a passion for problem-solving. A chess teacher with strong communication skills and a collaborative spirit, experienced in handling and conveying complex technical concepts. Polished analytical skills for effective debugging and algorithm design.             
+            </h4>
         </div>
     );
 }
