08cb494981a8a955ff1ab1b034eb3ebda5f9ffdb
Thomas Walker <thomasnwalker@yahoo.com>
2021-10-23 12:55:19 -0700

    contact form styling made more clean

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,16 +6,16 @@ function Contact(props){
             <h2 className="component-header"> Reach out</h2>
             <form className="contact-form">
             <div>
-                    <label>Name</label>
-                   <input name="name" />
+                    <label>Name</label> <br/>
+                   <input  className="form-field" name="name" />
                 </div> 
                 <div>
-                    <label>Email</label>
-                   <input name="email"/>
+                    <label>Email</label> <br/>
+                   <input className="form-field" name="email"/>
                 </div> 
                 <div>
-                    <label>Message</label>
-                   <textarea defaultValue="" name="message"></textarea>
+                    <label>Message</label> <br/>
+                   <textarea defaultValue="" name="message" className="message-field"></textarea>
                 </div> 
                 <button type="submit"> Send </button>
             </form>
