cfa726c733845c7c7d39b82c32c66efad6ef3801
Thomas Walker <thomasnwalker@yahoo.com>
2021-10-23 17:32:46 -0700

    descriptions added for promect thumbnails

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,23 +10,29 @@ function Project(){
          {title: "WeatherAll App",
         link: "https://twalker7.github.io/WeatherAll-pro/",
         repo: "https://github.com/twalker7/WeatherAll-pro",
-        //description: ,
+        description: "Fetches weather information from any city in the United States and returns it to a user-friendly front end." ,
         },
-        {title: "Password Generator",
+        {title: "PasswordWiz Generator",
             link: "https://twalker7.github.io/03-passwordWiz/",
             repo: "https://github.com/twalker7/03-passwordWiz" ,
-            //description: ,
+            description: "Prompts users for specific constraints and returns a password string that adheres to each specification selected.",
         },
         {title: "Quiz App",
             link: "https://twalker7.github.io/WeatherAll-pro/" ,
             repo: "https://github.com/twalker7/04-QuizTime",
-            //description: ,
+            description: "Administers a quiz that takes users through a set of 7 multiple choice questions, with buttons for selection.",
         },
         {
             title: "Paint-n-Sip",
             link:  "https://twalker7.github.io/HTeamL-Project/",
             repo: "https://github.com/twalker7/DIY-Paint-N-Sip",
-            //description: ,
+            description: "Assists users in the execution of a 'Paint and Sip' gathering event" ,
+        },
+        {
+            title: "Create Em",
+            link:  "https://create-em.herokuapp.com/",
+            repo: "https://github.com/abergtra/Create-Em-ecommerce",
+            description: "An e-commerce website for charcuterie boards. Users are able to customize boards for specific dietary restrictions." ,
         },
      ]);
     return(
@@ -40,7 +46,7 @@ function Project(){
                     <div className="project" id ={`project${i + 1}`}>
                         <h4><a href={project.link} target="_blank" rel="noreferrer">{project.title}</a>  </h4>
                         <h6><p className="project-repo"> <a href={project.repo} target="_blank" rel="noreferrer"> Repository Link</a></p> </h6>
-                        <p className = "project-description"> project description project description project description project description project description project description </p>
+                        <p className = "project-description"> {project.description} </p>
                     </div>
                 </li>
             )
