6d72b6248724aa16b8c140dd55f754d76b6196d1
Thomas Walker <thomasnwalker@yahoo.com>
2021-11-20 19:32:35 -0800

    imported react-bootstrap and placed Card element into About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from 'react-bootstrap';
 function About(){
     return(
         <div>
@@ -12,7 +13,21 @@ function About(){
             
             Full Stack Web Developer with experience in multiple programming languages and a passion for problem-solving. A chess teacher with strong communication skills and a collaborative spirit, experienced in handling and conveying complex technical concepts. Polished analytical skills for effective debugging and algorithm design.             
             </h6>
+        <Card style={{color : "#000", width: "400px" }}>
+            <Card.Body>
+                <Card.Title>
+                    Luhn Formula Credit Card Validator
+                </Card.Title>
+                <Card.Text style={{color: "red"}}> text sample lorem lorem ipsum baby </Card.Text>
+            </Card.Body>
+            <Card.Body>
+                <Card.Title>
+                    Code Snippet 2
+                </Card.Title>
+                <Card.Text style={{color: "red"}}> text sample lorem lorem ipsum baby </Card.Text>
+            </Card.Body>
 
+        </Card>
         </div>
     );
 }
