c87b79270cf289a9262122dda49e7d49e018687b
Thomas Walker <thomasnwalker@yahoo.com>
2021-12-17 19:09:22 -0800

    added technologies field to project objects in array

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Contact(props){
     return(
-        <div>
+        <div className="contact-page-div">
             <h2 className="component-header"> Get In Touch With Me Using the Links Below! </h2>
            
         </div>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -11,35 +11,40 @@ function Project(){
         alert('turn screen horizontally to view Projects section');
     }
     */
-
+    
      // array of project objects  above return 
      const [projects] = useState([
-         {title: "WeatherAll App",
-        link: "https://twalker7.github.io/WeatherAll-pro/",
-        repo: "https://github.com/twalker7/WeatherAll-pro",
-        description: "Fetches weather information from any city in the United States and returns it to a user-friendly front end." ,
+        {   title: "WeatherAll App",
+            link: "https://twalker7.github.io/WeatherAll-pro/",
+            repo: "https://github.com/twalker7/WeatherAll-pro",
+            description: "Fetches weather information from any city in the United States and returns it to a user-friendly front end." ,
+            technologies:"",
         },
-        {title: "PasswordWiz",
+        {   title: "PasswordWiz",
             link: "https://twalker7.github.io/03-passwordWiz/",
             repo: "https://github.com/twalker7/03-passwordWiz" ,
             description: "Prompts users for specific constraints and returns a password string that adheres to each specification selected.",
+            technologies:"",
         },
-        {title: "Quiz App",
+        {   title: "Quiz App",
             link: "https://twalker7.github.io/04-QuizTime/" ,
             repo: "https://github.com/twalker7/04-QuizTime",
             description: "Administers a quiz that takes users through a set of 7 multiple choice questions, with buttons for selection.",
+            technologies:"",
         },
         {
             title: "Paint-n-Sip",
             link:  "https://twalker7.github.io/DIY-Paint-N-Sip/",
             repo: "https://github.com/twalker7/DIY-Paint-N-Sip",
             description: "Assists users in the execution of a 'Paint and Sip' gathering event" ,
+            technologies:"",
         },
         {
             title: "Create Em",
             link:  "https://create-em.herokuapp.com/",
             repo: "https://github.com/abergtra/Create-Em-ecommerce",
             description: "An e-commerce website for charcuterie boards. Users are able to customize boards for specific dietary restrictions." ,
+            technologies:"",
         },
      ]);
 
