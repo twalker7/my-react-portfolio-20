d792d4436538e66ad230829620f3ec6e7c29b34e
Thomas Walker <thomasnwalker@yahoo.com>
2021-12-26 18:22:05 -0800

    added download link for resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
 
 function Resume(){
     return(
         <div>
            <br/>
             <img id="resume" alt="resume" src ={require(`../../assets/software-engineer-resume-thomaswalker.jpeg`).default} />
+            <br/>
+            <Button variant="link">
+            <a href={require("../../assets/software-engineer-resume-thomaswalker.pdf").default} download> Download Resume </a>
+            </Button>
         </div>
         
 
