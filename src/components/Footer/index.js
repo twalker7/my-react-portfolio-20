558a3a05fb0e88e55f566059e640c48649f49590
Thomas Walker <thomasnwalker@yahoo.com>
2022-01-04 18:00:34 -0800

    styled footer with icons and rearranged links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 function Footer(){
     return(
-        <h2 className="footer"> 
-            <a className="component-title" target="_blank" rel="noreferrer" href="https://github.com/twalker7">GitHub </a> | <a className="component-title" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/thomas-walker-7360ba8a/">LinkedIn</a> | <a className="component-title" target="_blank" rel="noreferrer" href="mailto: thomasnwalker@yahoo.com"> Email </a>
-        </h2>
+        <h5 className="footer"> 
+             <a className="component-title" target="_blank" rel="noreferrer" href="mailto: twalker@onetechadvanced.com"> twalker@onetechadvanced.com </a>
+             <br/> <br/>
+            <div>
+            <a className="component-title" target="_blank" rel="noreferrer" href="https://github.com/twalker7">
+                <img src={require(`../../assets/github-image.png`).default}/>
+              
+            </a> 
+            &emsp; &emsp;
+            <a className="component-title" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/thomas-walker-7360ba8a/">
+               <img src={require(`../../assets/linkedin-image.png`).default}/>
+                
+            </a> 
+            </div>
+        </h5>
     );
 }
 
